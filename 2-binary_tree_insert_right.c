d489ce741c03a6009fd118315a050a73b3ede9f4
Samuel Chen <sche842@gmail.com>
2026-04-01 16:12:53 +1100

    syntax fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
  * Return: pointer to the new node, or NULL on failure
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node;
 
