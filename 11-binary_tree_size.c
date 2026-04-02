cbc85fa45cb1078c47188f80653e2f092a3c2db2
Samuel Chen <sche842@gmail.com>
2026-04-02 13:24:17 +1100

    binary tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_size - find number of nodes
+ * @tree: pointer to root node
+ * Return: num of nodes in tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t size = 0;
+
+if (!tree)
+return (0);
+
+
+size += binary_tree_size(tree->left);
+size += binary_tree_size(tree->right);
+
+return (size + 1);
+}
